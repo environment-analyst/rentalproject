bf5aeab24026307f92e89c4d6765f806e86a75ec
rangmin <tkdals858@naver.com>
2024-03-28 09:30:12 +0900

    carvo

---
+++ src/main/java/vo/CarVO.java
@@ -0,0 +1,72 @@
+package vo;
+
+public class CarVO {
+	private int carNumber;
+	private String carName;
+	private String carCompany;
+	private int number;
+	private int carSeater;
+	private String carType;
+	private int carInsuranceAge;
+	private String carFuel;
+	private String carPrice;
+	
+	public int getCarNumber() {
+		return carNumber;
+	}
+	public void setCarNumber(int carNumber) {
+		this.carNumber = carNumber;
+	}
+	public String getCarName() {
+		return carName;
+	}
+	public void setCarName(String carName) {
+		this.carName = carName;
+	}
+	public String getCarCompany() {
+		return carCompany;
+	}
+	public void setCarCompany(String carCompany) {
+		this.carCompany = carCompany;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public int getCarSeater() {
+		return carSeater;
+	}
+	public void setCarSeater(int carSeater) {
+		this.carSeater = carSeater;
+	}
+	public String getCarType() {
+		return carType;
+	}
+	public void setCarType(String carType) {
+		this.carType = carType;
+	}
+	public int getCarInsuranceAge() {
+		return carInsuranceAge;
+	}
+	public void setCarInsuranceAge(int carInsuranceAge) {
+		this.carInsuranceAge = carInsuranceAge;
+	}
+	public String getCarFuel() {
+		return carFuel;
+	}
+	public void setCarFuel(String carFuel) {
+		this.carFuel = carFuel;
+	}
+	public String getCarPrice() {
+		return carPrice;
+	}
+	public void setCarPrice(String carPrice) {
+		this.carPrice = carPrice;
+	}
+	
+	
+	
+
+}
